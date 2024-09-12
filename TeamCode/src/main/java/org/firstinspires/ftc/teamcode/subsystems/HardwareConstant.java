5638e3a446d93367ffd460a813beffe2e0e28a71
Keerthika <keerthikanandagopal@gmail.com>
2024-09-12 18:25:15 -0500

    Hardware Constant

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HardwareConstant.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class HardwareConstant {
+    public final String LeftFrontMotor = "frontLeftMotor"; // Control Hub Port 1
+    public final String LeftBackMotor= "backLeftMotor";
+    public final String RightBackMotor= "backRightMotor";
+    public final String RightFrontMotor= "frontRightMotor";
+}
