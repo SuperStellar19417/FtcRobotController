326dce80f6990c018246111f9aa1a70ddc795d31
NagitoeYippieee <29essal@elmbrookstudents.org>
2024-10-08 18:21:01 -0500

    Initial class for outtake claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OuttakeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OuttakeClaw.java
@@ -19,6 +19,56 @@ public class OuttakeClaw {
         leftOuttakeClaw = hardwareMap.get(Servo.class, HardwareConstant.LeftOuttakeServo);
         wristOuttakeClaw = hardwareMap.get(Servo.class, HardwareConstant.WristOuttakeServo);
 
+        initOuttakeClaw();
+    }
+    public enum OUTTAKE_CLAW_SERVO_STATE{
+        OUTTAKE_CLAW_OPEN,
+
+        OUTTAKE_CLAW_CLOSE
+
+    }
+
+    public OUTTAKE_CLAW_SERVO_STATE outtakeClawServoState = OUTTAKE_CLAW_SERVO_STATE.OUTTAKE_CLAW_OPEN;
+
+    public enum OUTTAKE_CLAW_WRIST_STATE {
+
+        OUTTAKE_WRIST_DOWN,
+
+        OUTTAKE_WRIST_UP,
+
+    }
+
+    public OUTTAKE_CLAW_WRIST_STATE outtakeClawWristState = OUTTAKE_CLAW_WRIST_STATE.OUTTAKE_WRIST_DOWN;
+
+    public void initOuttakeClaw() {
+        rightOuttakeClaw.setPosition(0.00);;
+        leftOuttakeClaw.setPosition(0.00);
+        outtakeClawServoState = OUTTAKE_CLAW_SERVO_STATE.OUTTAKE_CLAW_CLOSE;
+
+        wristOuttakeClaw.setPosition(0.00);
+        outtakeClawWristState = OUTTAKE_CLAW_WRIST_STATE.OUTTAKE_WRIST_DOWN;
+    }
+
+    public void outtakeClawOpen(){
+        rightOuttakeClaw.setPosition(0.00);;
+        leftOuttakeClaw.setPosition(0.00);
+        outtakeClawServoState = OUTTAKE_CLAW_SERVO_STATE.OUTTAKE_CLAW_OPEN;
+    }
+
+    public void outtakeClawClose(){
+        rightOuttakeClaw.setPosition(0.00);;
+        leftOuttakeClaw.setPosition(0.00);
+        outtakeClawServoState = OUTTAKE_CLAW_SERVO_STATE.OUTTAKE_CLAW_CLOSE;
+    }
+
+    public void outtakeWristUp(){
+        wristOuttakeClaw.setPosition(0.00);
+        outtakeClawWristState = OUTTAKE_CLAW_WRIST_STATE.OUTTAKE_WRIST_UP;
+    }
+
+    public void outtakeWristDown(){
+        wristOuttakeClaw.setPosition(0.00);
+        outtakeClawWristState = OUTTAKE_CLAW_WRIST_STATE.OUTTAKE_WRIST_DOWN;
 
     }
 
