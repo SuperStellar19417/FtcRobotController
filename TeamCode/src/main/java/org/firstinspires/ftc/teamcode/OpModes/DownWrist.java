e367a5115869c10f81ebde3315ebd83c3fc7074c
NagitoeYippieee <29essal@elmbrookstudents.org>
2024-10-29 18:13:37 -0500

    Action classes for wrist

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/DownWrist.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.SubSystems.Claw;
+
+public class DownWrist implements Action {
+    public OpMode opMode;
+
+    public DownWrist(OpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    Claw claw = new Claw(opMode);
+
+    @Override
+    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+        claw.intakeClawDown();
+        return true;
+    }
+}
