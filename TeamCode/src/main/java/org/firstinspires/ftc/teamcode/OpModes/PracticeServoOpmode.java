73c4bd99ffe5d1c2a2693cbe38b4cb05a9e15f5b
aditi <26moorca@elmbrookstudents.org>
2024-12-03 18:09:26 -0600

    instruction comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PracticeServoOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PracticeServoOpmode.java
@@ -4,6 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class PracticeServoOpmode extends LinearOpMode {
 
+    /*
+     *
+     * add two positions + initial position + functions to get them to initial and both positions
+     * runOpmode function that allows the user to use the gamepad to access all these positions
+     * telemetry updates for each position
+     */
     @Override
     public void runOpMode() throws InterruptedException {
 
