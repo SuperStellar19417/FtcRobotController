3c0fabfe2470dc74cc66b2bfe860d97d52da00be
aditi <26moorca@elmbrookstudents.org>
2024-12-03 18:05:55 -0600

    test opmodes for juniors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PracticeDcMotorOpmode.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class PracticeDcMotorOpmode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
