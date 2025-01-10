b2b1a295c7ff782ceece85f475aa2acaeacf5362
Keerthika <keerthikanandagopal@gmail.com>
2025-01-09 19:36:09 -0600

    Limelight

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/practiceOpmodes/LimelightOpmode.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.OpModes.practiceOpmodes;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.SubSystems.GamepadController;
+
+import java.util.List;
+
+@TeleOp(name = "Practice DC Motor", group = "Tests")
+public class LimelightOpmode extends LinearOpMode {
+    private Limelight3A limelight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+        limelight.start();
+        while (opModeIsActive()) {
+
+            if (isStopRequested())
+                break;
+            LLResult result = limelight.getLatestResult();
+
+            if (result != null) {
+                if (result.isValid()) {
+                    // Access fiducial results
+                    List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+                    for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                        telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(),fr.getTargetXDegrees(), fr.getTargetYDegrees());
+                    }
+
+                    // Access color results
+                    List<LLResultTypes.ColorResult> colorResults = result.getColorResults();
+                    for (LLResultTypes.ColorResult cr : colorResults) {
+                        telemetry.addData("Color", "X: %.2f, Y: %.2f", cr.getTargetXDegrees(), cr.getTargetYDegrees());
+                    }
+                }
+
+
+
+            }
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+
+    }
+}
