cea42abdc5813760a0a783a9b2a2518ed91bc2ba
Aarushi <26banera@elmbrookstudents.org>
2025-02-17 22:53:30 -0600

    Meepmeep two trajectories

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,19 +1,21 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(300);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, -72, 0))
                       // .forward(30)
                         //.turn(Math.toRadians(90))
                         //.forward(30)
@@ -22,10 +24,28 @@ public class MeepMeepTesting {
                         //.turn(Math.toRadians(90))
                         //.forward(30)
                         //.turn(Math.toRadians(90))
-                        .turn(Math.toRadians(90))
-                        .forward(20)
-
+                        //basket
+                        .turn(Math.toRadians(180))
+                        /*.forward(48)
+                        .strafeTo(new Vector2d(0.0, -37.0))
+                        .turn(Math.toRadians(-90))
+                        .strafeTo(new Vector2d(-48.0, -58.0))
+                        .turn(Math.toRadians(90)) */
                         .build());
+
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeBlueDark() )
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, -72, 0))
+                        .strafeLeft(38)
+                        .turn(Math.toRadians(90) )
+                        .back(38)
+                        .strafeRight(30)
+                        .turn(Math.toRadians(-90))
+                        .back(30)
+                        .build());
+
 
 
 
@@ -33,6 +53,7 @@ public class MeepMeepTesting {
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
+                .addEntity(myBot2)
                 .start();
 
 
