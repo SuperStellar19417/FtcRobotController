46f37770aacf3db0482c0a0f0a859ac8344feb6b
Aarushi <26banera@elmbrookstudents.org>
2025-02-24 16:53:16 -0600

    Meepmeep specimen

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,37 +12,10 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(300);
 
-        final Pose2d startPose = new Pose2d(new Vector2d(-2, 60), 90);
-         final Vector2d dropPose = new Vector2d(-55, -50); //132
-        //  private final Vector2d dropPoseAdjust = new Vector2d(10, 33.5); //132
-        // private final Vector2d dropPoseAdjust2 = new Vector2d(17, 30);
-       //  final Vector2d midPoseCycle = new Vector2d(-46, -50); //90
-         final Vector2d cycle1 = new Vector2d(-41, -39); //50
-         final Vector2d cycle2 = new Vector2d(-53, -39); //40
-         final Vector2d midPoseSub = new Vector2d(-52, -45); //90
-         final Vector2d parkPose = new Vector2d(-27, -13); //90
-
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, -72, 90))
-                .strafeTo(dropPose)
-                .turn(Math.toRadians(135))
-          //      .strafeTo(midPoseCycle)
-                .turn(Math.toRadians(-135))
-                .strafeTo(cycle1)
-                .turn(Math.toRadians(40))
-                .strafeTo(dropPose)
-                .turn(Math.toRadians(95))
-           //     .strafeTo(midPoseCycle)
-                .strafeTo(cycle2)
-                .turn(Math.toRadians(-135))
-                .turn(Math.toRadians(40))
-                .strafeTo(dropPose)
-                .turn(Math.toRadians(95))
-                .turn(Math.toRadians(-55))
-                .strafeTo(midPoseSub)
-                .strafeTo(parkPose)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-8, -72, 0))
                       // .forward(30)
                         //.turn(Math.toRadians(90))
                         //.forward(30)
@@ -52,7 +25,7 @@ public class MeepMeepTesting {
                         //.forward(30)
                         //.turn(Math.toRadians(90))
                         //basket
-                      //  .turn(Math.toRadians(180))
+                        .turn(Math.toRadians(180))
                         /*.forward(48)
                         .strafeTo(new Vector2d(0.0, -37.0))
                         .turn(Math.toRadians(-90))
@@ -64,13 +37,10 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setColorScheme(new ColorSchemeBlueDark() )
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, -72, 0))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-8, -72, 0))
                         .strafeLeft(38)
                         .turn(Math.toRadians(90) )
-                        .back(38)
                         .strafeRight(30)
-                        .turn(Math.toRadians(-90))
-                        .back(30)
                         .build());
 
 
