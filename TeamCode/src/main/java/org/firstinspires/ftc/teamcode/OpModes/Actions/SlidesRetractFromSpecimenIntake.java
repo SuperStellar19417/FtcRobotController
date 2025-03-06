0c362f63cbc43394275be11f1b790264ca2d3d4a
aditi <26moorca@elmbrookstudents.org>
2025-03-06 08:09:43 -0600

    specimen intake slide positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Actions/SlidesRetractFromSpecimenIntake.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.OpModes.Actions;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.SubSystems.IntakeSlide;
+
+public class SlidesRetractFromSpecimenIntake implements Action {
+    private IntakeSlide intakeSlide;
+    private boolean initialized = false;
+    private Telemetry telemetry;
+
+
+    public SlidesRetractFromSpecimenIntake(IntakeSlide intakeSlide, Telemetry telemetry){
+        this.intakeSlide = intakeSlide;
+        this.telemetry = telemetry;
+    }
+
+    @Override
+    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+        // powers on motor, if it is not on
+        if (!initialized) {
+            intakeSlide.retractSlideFromSpecIntake();
+            initialized = true;
+        }
+
+        // checks arm and slide current position
+        double pos = intakeSlide.slideMotor.getCurrentPosition();
+
+        telemetry.addData("Slide pos:", pos);
+        telemetry.update();
+
+        // giving some buffer here for ticks
+        if (pos > intakeSlide.SLIDE_SPEC_INTAKE_DELTA + 50) {
+            // true causes the action to rerun
+            return true;
+        } else {
+            telemetry.addLine("Slides reached specimen");
+            telemetry.update();
+            // false stops action rerun
+            return false;
+        }
+        // overall, the action powers the slides until it surpasses
+        // SLIDE_POSITION_MAX encoder ticks, then powers it off
+
+    }
+}
